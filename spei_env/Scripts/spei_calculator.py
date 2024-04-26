6f75f88f4cc1b8df7a1382a1bfd8c21e2412cc6e
james-buckley2001 <jameshb7923@gmail.com>
2024-04-26 17:59:37 +0100

    aggregation_function

--- spei_env/Scripts/spei_calculator.py
+++ spei_env/Scripts/spei_calculator.py
@@ -1,43 +1,92 @@
 import pandas as pd
 from pathlib import Path
+from dataclasses import dataclass, field
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-#eggghjdnvldjvvdk;m
-class DataStorage:
-    def __init__(self):
-        self.possible_acculmulation_periods = [1,3,6,12,18,24] # in months
-        self.gld_time_period = [1961,2022] # gld = general logistic distributiom
+@dataclass
+class DataStorage():
+    possible_acculmulation_periods:list[float] = field(default_factory=list)
+    gld_time_period : list[float] = field(default_factory=list)
+    _wd : Path = Path(r'C:\Users\BUCKLEJ1\OneDrive - Jacobs\Documents\SPEI Application\data_from_ea\Task 2 SPI SPEI\Input files')
+    _rainfall_input_data_file_name : str = r'WSR_HydAreas_HadUK_v1_2_0_0_1871_ForRainfallAnalysis.xlsx'
+    _pet_input_data_file_name : str = r'WSR_EA-PET_1961_HydAreasForGrassPET_Eng.xlsx'
+    _start_year : str = r'1961'
+    _end_year :str = r'2022'
 
-        self._wd = Path(r'C:\Users\BUCKLEJ1\OneDrive - Jacobs\Documents\SPEI Application\data_from_ea\Task 2 SPI SPEI\Input files')
-        self._rainfall_input_data_path = self._wd / r'WSR_HydAreas_HadUK_v1_2_0_0_1871_ForRainfallAnalysis.xlsx'
-        self._pet_input_data_path = self._wd / r'WSR_EA-PET_1961_HydAreasForGrassPET_Eng.xlsx' #within hydrological areas
+    def __post_init__(self):
+        self.gld_time_period = [1961,2022]
+        self.possible_acculmulation_periods = [1,3,6,12,18,24,36]
 
-
-#jbj
 class SpeiCalculator(DataStorage):
     """
     Inputs: gridded rainfall data, PET within hydrometric areas data, acculmulation period
 
     Outputs: SPEI grids
     """
-    def __init__(self):
+    def __init__(self, acculmulation_period:float):
         super().__init__()
+        self.acculmulation_period = acculmulation_period
         pass
 
-    def import_gridded_rainfall(self):
-        self.rainfall_data = pd.read_excel(self._rainfall_input_data_path, header=[0, 1])
-
-    def import_hydrometric_areas_pet_data(self):
-        self.pet_data = pd.read_excel(self._pet_input_data_path, header=[0, 1])
+    def import_data(self, file_name):
+        df = pd.read_excel(self._wd / file_name, header=[0, 1])
+        if 'year' in df.columns:
+            df.rename(columns={'year': 'Year'}, inplace=True)
+        if 'month' in df.columns:
+            df.rename(columns={'month': 'Month'}, inplace=True)
+        headers = df.columns
+        df.columns = df.columns.droplevel(1)
+        return df, headers
 
     def import_input_data(self):
-        self.import_hydrometric_areas_pet_data()
-        self.import_gridded_rainfall()
+        self.rainfall_data, self.rainfall_data_headers =  self.import_data(file_name = self._wd / self._rainfall_input_data_file_name)
+        self.pet_data, self.pet_data_headers = self.import_data(file_name = self._wd / self._pet_input_data_file_name)
 
-    def aggregate_data_for_acculmulation_period(self, acculmulation_period: list) -> pd.DataFrame: 
-        
-        pass
+    def generate_aggregation_dates(self,start_date, end_date):
+        start_date_obj = start_date
+        dates_list = []
+        while start_date_obj <= end_date:
+            new_date = start_date_obj.replace(year=start_date_obj.year + 1)
+            dates_list.append(new_date)
+            start_date_obj = new_date
+        return dates_list
 
-    def calculate_water_balance(self): #may not need to do this
+    def aggregate_data_for_acculmulation_period(self, df = None) -> pd.DataFrame: 
+        df = self.rainfall_data
+        df['Date'] = pd.to_datetime(df[['Year', 'Month']].assign(day=1))
+
+        list_of_time_series = []
+        for month in range(1,13):
+            start_date = datetime.strptime(f'{self._start_year}-{month:02d}-01', '%Y-%m-%d')
+            end_date = datetime.strptime(f'{self._end_year}-{month:02d}-01', '%Y-%m-%d')
+
+            aggregation_start_dates = self.generate_aggregation_dates(start_date, end_date)
+            aggregation_end_dates = [date - relativedelta(months=self.acculmulation_period-1) for date in aggregation_start_dates]
+            aggregation_dates_df = pd.DataFrame({
+                                        'aggregation_start_dates': aggregation_start_dates,
+                                        'aggregation_end_dates': aggregation_end_dates
+                                    })
+
+            averaged_data = []
+            for _, row in aggregation_dates_df.iterrows():
+                df_filtered = df[(df['Date'] <= row['aggregation_start_dates']) & (df['Date'] >= row['aggregation_end_dates'])]
+                numeric_mean = df_filtered.mean(numeric_only=True)
+                columns_to_keep = df_filtered.columns
+                mean_df = pd.DataFrame(columns=columns_to_keep)
+                mean_df.loc[0] = numeric_mean
+                mean_df.drop(columns=['Year', 'Month', 'Status'], inplace=True)
+                mean_df['Date'] = row['aggregation_start_dates']
+                averaged_data.append(mean_df)
+
+            time_series_df =  pd.concat(averaged_data, axis=0)
+            list_of_time_series.append(time_series_df)
+
+        self.list_of_time_series = list_of_time_series
+
+ 
+
+    def calculate_water_balance(self):
         #pet = potential evapotranspiration
         pass
 
@@ -46,6 +95,8 @@ class SpeiCalculator(DataStorage):
 
 
 if __name__ == '__main__':
-    spei_calc = SpeiCalculator()
+
+    spei_calc = SpeiCalculator(acculmulation_period= 6)
     spei_calc.import_input_data()
+    spei_calc.aggregate_data_for_acculmulation_period(df = spei_calc.rainfall_data[0:4])
     print('egg')
